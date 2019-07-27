1f3dde7d6e6383159d326f06961a5cc8ea59982f
Aravindh B <aravinddhiva@gmail.com>
2019-07-27 15:44:13 +0530

    Library mangement

---
+++ precache-manifest.67c8c531d26bce5e88d3b2e94c90f398.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1c8c69cb239462a7aeb521f08c08dd",
+    "url": "/PortFolio/index.html"
+  },
+  {
+    "revision": "ea00137eb9e02a7a47e6",
+    "url": "/PortFolio/static/js/2.b9e182a4.chunk.js"
+  },
+  {
+    "revision": "1f44d56d7342efb907a6",
+    "url": "/PortFolio/static/js/main.4983d6ce.chunk.js"
+  },
+  {
+    "revision": "0e3f674c2e2d5f68e62c",
+    "url": "/PortFolio/static/js/runtime~main.4aeb350c.js"
+  }
+]);
